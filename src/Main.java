b38949e3936dc60e665bf15687b97c2b73e543aa
russa89 <natast2189@gmail.com>
2024-04-02 15:20:18 +0300

    first 3 tasks are done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nTask1");
+
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, " +
+                    "нужно немного подождать."); }
+            System.out.println("\nTask2");
+
+        int temperature = 6;
+        if (temperature <= 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+
+        System.out.println("\nTask3");
+
+        int speed = 59;
+        if (speed > 60) {
+            System.out.println("Если скорость равна " + speed + ", то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость равна " + speed + ", то можно ездить спокойно");
+        }
+
+        System.out.println("\nTask4");
+
     }
 }
